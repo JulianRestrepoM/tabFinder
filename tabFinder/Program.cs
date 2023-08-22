a13bbb794f3ff944f3408ad70c31f8d07549e971
Julian <julianres28@hotmail.com>
2023-08-22 22:28:58 +0200

    can get authen token and first get to playlist

--- tabFinder/Program.cs
+++ tabFinder/Program.cs
@@ -1,25 +1,46 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Unicode;
 using Microsoft.AspNetCore.Mvc.Formatters;
 
 Console.WriteLine("Welcome to TAB Finder!");
 
 HttpClient client = new();
 
-string clientID = "6e91c3589c6040da9d24780524804018";
+//get Spotify Access token
+string clientId = "6e91c3589c6040da9d24780524804018";
 string clientSecret = "12e41af7e4be4e3e8d7907f2bffc1fa2";
 
-string tokenRequest = "grant_type=client_credentials&client_id="+clientID+"&client_secret="+clientSecret;
+string tokenRequest = "grant_type=client_credentials&client_id="+clientId+"&client_secret="+clientSecret;
 
 
-client.BaseAddress = new Uri("https://accounts.spotify.com/api/");
+// client.BaseAddress = new Uri("https://accounts.spotify.com/api/");
 
 var content = new StringContent(tokenRequest, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
 
-var response = client.PostAsync("token", content).Result;
+var response = client.PostAsync("https://accounts.spotify.com/api/token", content).Result;
 
 var responseContent = response.Content.ReadAsStringAsync().Result;
+
+Token accessToken = JsonSerializer.Deserialize<Token>(responseContent);
+// Console.WriteLine(responseContent);
+Console.WriteLine("Access token = "+ accessToken.access_token);
+
+/*************************************************/
+
+string playlistId = "5qUj0OW96Y0Vyc3SUlXY6B"; //Julian's Stoner metal +
+string limit = "1";
+string getTotalUri = $"https://api.spotify.com/v1/playlists/{playlistId}/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29%2Ctotal%2Climit&limit={limit}";
+
+var message = new HttpRequestMessage(HttpMethod.Get, getTotalUri);
+message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.access_token);
+
+response = client.SendAsync(message).Result;
+
+responseContent = response.Content.ReadAsStringAsync().Result;
+
 Console.WriteLine(responseContent);
 
-Console.WriteLine("Hello World!");
 

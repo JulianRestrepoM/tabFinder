ed016213680c23611288a36d86b2d1274d979686
Julian <julianres28@hotmail.com>
2023-08-25 18:50:40 +0200

    refractored code

--- tabFinder/Program.cs
+++ tabFinder/Program.cs
@@ -9,99 +9,13 @@ using System.Threading;
 Console.WriteLine("Welcome to TAB Finder!");
 
 HttpClient client = new();
+SpotifyApi spotifyApi= new SpotifyApi(client);
 
-//get Spotify Access token
-string clientId = "6e91c3589c6040da9d24780524804018";
-string clientSecret = "12e41af7e4be4e3e8d7907f2bffc1fa2";
-
-string tokenRequest = "grant_type=client_credentials&client_id="+clientId+"&client_secret="+clientSecret;
-
-
-// client.BaseAddress = new Uri("https://accounts.spotify.com/api/");
-
-var content = new StringContent(tokenRequest, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
-
-var response = client.PostAsync("https://accounts.spotify.com/api/token", content).Result;
-
-var responseContent = response.Content.ReadAsStringAsync().Result;
-
-Token accessToken = JsonSerializer.Deserialize<Token>(responseContent);
-
-Console.WriteLine("Access token = "+ accessToken.AccessToken);
-
-/*************************************************/
-//make first call to playlist API, and save them to objects.
-//record the total amount of songs in the playlist.
+Token accessToken = spotifyApi.GetAccessToken();
 
 string playlistId = "5qUj0OW96Y0Vyc3SUlXY6B"; //Julian's Stoner metal +
-int limit = 1;
-int offset = 0;
-string getTotalUri = $"https://api.spotify.com/v1/playlists/{playlistId}/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29%2Ctotal%2Climit&limit={limit}&offset={offset}";
 
-var message = new HttpRequestMessage(HttpMethod.Get, getTotalUri);
-message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.AccessToken);
-
-response = client.SendAsync(message).Result;
-
-responseContent = response.Content.ReadAsStringAsync().Result;
-
-SpotifyPlaylistResponse spotifyResponse = JsonSerializer.Deserialize<SpotifyPlaylistResponse>(responseContent);
-
-Console.WriteLine(responseContent);
-
-Console.WriteLine($"response total = {spotifyResponse.Total}");
-
-/********************************************************/
-//get all songs in a playlist and then store them in a list
-
-List<Item> songList= new();
-int remainingSongs = spotifyResponse.Total;
-const int maxLimit = 50;
-
-while(remainingSongs > 0) {
-    Console.WriteLine($"REMAING SONGS = {remainingSongs}");
-    Thread.Sleep(1000); //can cancell if infinite loop
-    if(remainingSongs >= maxLimit) {
-        limit = maxLimit;
-        getTotalUri = $"https://api.spotify.com/v1/playlists/{playlistId}/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29%2Ctotal%2Climit&limit={limit}&offset={offset}";
-
-        message = new HttpRequestMessage(HttpMethod.Get, getTotalUri);
-        message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.AccessToken);
-
-        response = client.SendAsync(message).Result;
-
-        responseContent = response.Content.ReadAsStringAsync().Result;
-
-        spotifyResponse = JsonSerializer.Deserialize<SpotifyPlaylistResponse>(responseContent);
-
-        foreach(Item i in spotifyResponse.Items) {
-            songList.Add(i);
-        }
-        remainingSongs -= limit;
-        offset += limit;
-    }
-    else {
-        limit = remainingSongs;
-        getTotalUri = $"https://api.spotify.com/v1/playlists/{playlistId}/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29%2Ctotal%2Climit&limit={limit}&offset={offset}";
-
-        message = new HttpRequestMessage(HttpMethod.Get, getTotalUri);
-        message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.AccessToken);
-
-        response = client.SendAsync(message).Result;
-
-        responseContent = response.Content.ReadAsStringAsync().Result;
-
-        spotifyResponse = JsonSerializer.Deserialize<SpotifyPlaylistResponse>(responseContent);
-
-        foreach(Item i in spotifyResponse.Items) {
-            songList.Add(i);
-        }
-        remainingSongs -= limit;
-        Console.WriteLine($"Remaining songs = {remainingSongs}");
-    }
-}
-
-
+List<Item> songList = spotifyApi.GetSongList(playlistId, accessToken);
 
 foreach(Item i in songList) {
     string artists = $"{i.Track.Artists[0].Name}";
--- tabFinder/SpotifyAPI.cs
+++ tabFinder/SpotifyAPI.cs
@@ -1,4 +1,8 @@
 using System.Text.Json.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 
 public class Artist {
@@ -34,5 +38,77 @@ class Token {
     public required string TokenType {get; set;}
     [JsonPropertyName("expires_in")]
     public int ExpiresIn{get; set;}
+}
+
+class SpotifyApi {
+    private const string ClientId = "6e91c3589c6040da9d24780524804018";
+    private const string ClientSecret = "12e41af7e4be4e3e8d7907f2bffc1fa2";
+    private const int maxLimit = 50;
+    private HttpClient client;
+    public SpotifyApi(HttpClient client) {
+        this.client = client;
+    }
+
+
+    public Token GetAccessToken() {
+        string tokenRequest = "grant_type=client_credentials&client_id="+ClientId+"&client_secret="+ClientSecret;
+
+        var content = new StringContent(tokenRequest, System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+
+        var response = client.PostAsync("https://accounts.spotify.com/api/token", content).Result;
+
+        var responseContent = response.Content.ReadAsStringAsync().Result;
+
+        Token accessToken = JsonSerializer.Deserialize<Token>(responseContent);
+
+        return accessToken;
+    }
+
+    private SpotifyPlaylistResponse sendHttpGet(string playlistId, int limit, int offset, Token accessToken) {
+        string completeUri = $"https://api.spotify.com/v1/playlists/{playlistId}/tracks?fields=items%28track%28name%2Cartists%28name%29%29%29%2Ctotal%2Climit&limit={limit}&offset={offset}";
+        
+        var message = new HttpRequestMessage(HttpMethod.Get, completeUri);
+        message.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.AccessToken);
+
+        var response = client.SendAsync(message).Result;
+
+        var responseContent = response.Content.ReadAsStringAsync().Result;
+
+        SpotifyPlaylistResponse spotifyResponse = JsonSerializer.Deserialize<SpotifyPlaylistResponse>(responseContent);
+        return spotifyResponse;
+        
+    }
+
+    private int GetSongAmount(string playlistId, Token accessToken) {
+            
+            SpotifyPlaylistResponse spotifyResponse = sendHttpGet(playlistId, 1, 0, accessToken);
+            
+            return spotifyResponse.Total;
+    }
+
+    public List<Item> GetSongList(string playlistId, Token accessToken) {
+        List<Item> songList= new();
+        int remainingSongs = GetSongAmount(playlistId, accessToken);
+        int offset = 0;
+        SpotifyPlaylistResponse spotifyResponse;
+        
+        while(remainingSongs > 0) {
+            Console.WriteLine($"REMAING SONGS = {remainingSongs}");
+            Thread.Sleep(1000); //can cancel if infinite loop
+            if(remainingSongs >= maxLimit) {
+                spotifyResponse = sendHttpGet(playlistId, maxLimit, offset, accessToken);
+                remainingSongs -= maxLimit;
+                offset += maxLimit;
+            }
+            else {
+                spotifyResponse = sendHttpGet(playlistId, remainingSongs, offset, accessToken);
+                remainingSongs = 0;
+            }
+            foreach(Item i in spotifyResponse.Items) {
+                    songList.Add(i);
+                }
+        }
+        return songList; 
+    }
 }
 
